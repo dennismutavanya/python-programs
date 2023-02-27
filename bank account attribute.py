34928baeb856c3aa0b7e3b0c2a1a24699ae14a0c
DennisMutavanya <102258784+dennismutavanya@users.noreply.github.com>
2023-02-27 15:45:31 +0300

    Update bank account attribute.py

--- bank account attribute.py
+++ bank account attribute.py
@@ -1,35 +1,44 @@
-import datetime
-
 class BankAccount:
-    def __init__(self, account_number, balance, customer_name):
+    def __init__(self, account_number, balance, date_of_opening, customer_name):
         self.account_number = account_number
         self.balance = balance
-        self.date_of_opening = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.date_of_opening = date_of_opening
         self.customer_name = customer_name
 
     def deposit(self, amount):
-        if amount < 0:
-            return "Invalid amount. Please enter a positive value."
-        else:
-            self.balance += amount
-            return f"Deposit successful. New balance: {self.balance}"
+        self.balance += amount
+        return amount
 
     def withdraw(self, amount):
-        if amount < 0:
-            return "Invalid amount. Please enter a positive value."
-        elif amount > self.balance:
-            return "Insufficient funds. Please try a smaller amount."
+        if self.balance < amount:
+            return "Insufficient balance"
         else:
             self.balance -= amount
-            return f"Withdrawal successful. New balance: {self.balance}"
+            return amount
 
     def check_balance(self):
-        return f"Current balance: {self.balance}"
+        print("Current balance:", self.balance)
 
     def customer_details(self):
-        return f"Account number: {self.account_number}, Customer name: {self.customer_name}, Date of opening: {self.date_of_opening}"
-account = BankAccount("1234567890", 1000, "John Doe")
-print(account.deposit(500))
-print(account.withdraw(200))
-print(account.check_balance())
-print(account.customer_details())
+        print("Customer Name:", self.customer_name)
+        print("Account Number:", self.account_number)
+        print("Date of Opening:", self.date_of_opening)
+        print("Current Balance:", self.balance)
+
+
+# Example usage
+my_account = BankAccount("123456", 1000, "2022-01-01", "John Doe")
+
+deposit_amount = my_account.deposit(500)
+print("Deposited amount:", deposit_amount)
+
+my_account.check_balance()
+
+withdraw_amount = my_account.withdraw(2000)
+print("Withdrawn amount:", withdraw_amount)
+
+withdraw_amount = my_account.withdraw(1000)
+print("Withdrawn amount:", withdraw_amount)
+
+my_account.check_balance()
+my_account.customer_details()
